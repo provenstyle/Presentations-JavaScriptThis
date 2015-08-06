04f10589b012e55855f9b20e157a55983bd10c39
michael dudley <michael.dudley@improvingenterprises.com>
2015-08-06 13:30:21 -0500

    Added a callback example

--- source/javaScriptThis/javaScriptThis/app/insideAFunction/asACallback.js
+++ source/javaScriptThis/javaScriptThis/app/insideAFunction/asACallback.js
@@ -1,1 +1,31 @@
-﻿
+﻿var Book = function (title, author) {
+    this.title = title;
+    this.author = author;
+    this.read = function(book) {
+        console.log("Reading " + book.title + ' by ' + book.author);
+    };
+    this.readAllBooks = function(books) {
+        _.each(books, function (book) {
+            this.read(book);
+        }, this);
+    }
+};
+
+var mistbornSeries = [
+    new Book('Mistborn', 'Brandon Sanderson'),
+    new Book('The Well of Assension', 'Brandon Sanderson'),
+    new Book('The Hero of Ages', 'Brandon Sanderson')
+];
+
+var book = new Book();
+book.readAllBooks(mistbornSeries);
+
+//======================================
+
+$(function () {
+    var button = $('.js-button');
+    button.click(function() {
+        console.log(this);
+        console.log(this === button[0]);
+    });
+});
