b38fb3d0cb76b015bd6e91892c067372623d8eb1
michael dudley <michael.dudley@improvingenterprises.com>
2015-08-06 17:13:39 -0500

    Setup

--- source/javaScriptThis/javaScriptThis/app/insideAFunction/withCall.js
+++ source/javaScriptThis/javaScriptThis/app/insideAFunction/withCall.js
@@ -1,20 +1,35 @@
-﻿function getInfo() {
-    return this.title + ' by ' + this.author;
-}
-
-var Book = function (title, author) {
+﻿var Book = function (title, author) {
     this.title = title;
     this.author = author;
     this.read = function () {
-        console.log("Reading " + getInfo.call(this));
+        console.log("Reading " + this.title + ' by ' + this.author);
     };
-    this.toString = getInfo;
 };
 
-var book = new Book("The Way of King", "Brandon Sanderson");
+var book = new Book("The Way of Kings", "Brandon Sanderson");
 book.read();
 
-console.log(book.toString());
-
 var book = new Book("Words of Radiance", "Brandon Sanderson");
-console.log(getInfo.call(book));
+
+////======================================
+
+//function getInfo() {
+//    return this.title + ' by ' + this.author;
+//}
+
+//var Book = function (title, author) {
+//    this.title = title;
+//    this.author = author;
+//    this.read = function () {
+//        console.log("Reading " + getInfo.call(this));
+//    };
+//    this.toString = getInfo;
+//};
+
+//var book = new Book("The Way of Kings", "Brandon Sanderson");
+//book.read();
+
+//console.log(book.toString());
+
+//var book = new Book("Words of Radiance", "Brandon Sanderson");
+//console.log(getInfo.call(book));
